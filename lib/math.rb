94f3e2745b18d334b6614ffdca32b3d287afcffc
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-04 19:35:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -27,3 +27,27 @@
 #   square = Math.sqrt(num)
 #   square
 # end
+
+def addition(num1, num2)
+  num1 + num2
+end
+
+def subtraction(num1, num2)
+  num1 - num2
+end
+
+def division(num1, num2)
+  num1 / num2
+end
+
+def multiplication(num1, num2)
+  num1 * num2
+end
+
+def modulo(num1, num2)
+  num1 % num2
+end
+
+def square_root(num)
+  Math.sqrt(num)
+end
